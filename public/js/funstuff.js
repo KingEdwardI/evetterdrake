9402cd1c8827191d0fb2425da5d70be7b48a5db2
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2016-12-03 22:05:54 -0700

    added console messages

--- public/js/funstuff.js
+++ public/js/funstuff.js
@@ -1,8 +1,53 @@
-console.log('is DevTools open?', window.devtools.open);
-console.log('and DevTools orientation?', window.devtools.orientation);
-
 window.addEventListener('devtoolschange', function (e) {
-  console.log('is Devtools open?', e.detail.open);
-  console.log('and Devtools orientation?', e.detail.orientation);
 
+  if(e.detail.orientation == "Horizontal") {
+  console.log('##########    ####         ####    ###########');
+  console.log('##########    ####         ####    ############');
+  console.log('####           ####       ####     ####     ####');
+  console.log('####           ####       ####     ####      ####');
+  console.log('##########      ####     ####      ####       ####');
+  console.log('##########      ####     ####      ####       ####');
+  console.log('####             ####   ####       ####      ####');
+  console.log('####             ####   ####       ####     ####');
+  console.log('##########        #########        ############');
+  console.log('##########         #######         ###########');
+  console.log('');
+  }
+
+  else {
+  console.log('##########');
+  console.log('##########');
+  console.log('####      ');
+  console.log('####      ');
+  console.log('##########');
+  console.log('##########');
+  console.log('####      ');
+  console.log('####      ');
+  console.log('##########');
+  console.log('##########');
+  console.log('')
+  console.log('####         ####');
+  console.log('####         ####');
+  console.log(' ####       #### ');
+  console.log(' ####       #### ');
+  console.log('  ####     ####  ');
+  console.log('  ####     ####  ');
+  console.log('   ####   ####   ');
+  console.log('   ####   ####   ');
+  console.log('    #########    ');
+  console.log('     #######     ');
+  console.log('')
+  console.log('###########    ');
+  console.log('############   ');
+  console.log('####     ####  ');
+  console.log('####      #### ');
+  console.log('####       ####');
+  console.log('####       ####');
+  console.log('####      #### ');
+  console.log('####     ####  ');
+  console.log('############   ');
+  console.log('###########    ');
+  }
+  console.log('');
+  console.log('Thanks for taking the time to check out my scripts.'); // add 'try calling funfunctio()....'
 })
