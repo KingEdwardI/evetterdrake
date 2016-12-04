11d8663e2380ad60dbae3b90eaa91649498ff2f5
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2016-12-03 22:45:49 -0700

    colors

--- public/js/funstuff.js
+++ public/js/funstuff.js
@@ -15,38 +15,38 @@ window.addEventListener('devtoolschange', function (e) {
   }
 
   else {
-  console.log('##########');
-  console.log('##########');
-  console.log('####      ');
-  console.log('####      ');
-  console.log('##########');
-  console.log('##########');
-  console.log('####      ');
-  console.log('####      ');
-  console.log('##########');
-  console.log('##########');
+  console.log('%c##########','color: #8A00E5');
+  console.log('%c##########','color: #7F0CCE');
+  console.log('%c####      ','color: #7418B7');
+  console.log('%c####      ','color: #6924A0');
+  console.log('%c##########','color: #5E3089');
+  console.log('%c##########','color: #533D72');
+  console.log('%c####      ','color: #48495B');
+  console.log('%c####      ','color: #3D5544');
+  console.log('%c##########','color: #32612D');
+  console.log('%c##########','color: #276D16');
   console.log('')
-  console.log('####         ####');
-  console.log('####         ####');
-  console.log(' ####       #### ');
-  console.log(' ####       #### ');
-  console.log('  ####     ####  ');
-  console.log('  ####     ####  ');
-  console.log('   ####   ####   ');
-  console.log('   ####   ####   ');
-  console.log('    #########    ');
-  console.log('     #######     ');
+  console.log('%c####         ####','color: #E50300');
+  console.log('%c####         ####','color: #D21719');
+  console.log('%c ####       #### ','color: #BF2B32');
+  console.log('%c ####       #### ','color: #AC3F4C');
+  console.log('%c  ####     ####  ','color: #9A5365');
+  console.log('%c  ####     ####  ','color: #87677F');
+  console.log('%c   ####   ####   ','color: #747B98');
+  console.log('%c   ####   ####   ','color: #628FB1');
+  console.log('%c    #########    ','color: #4FA3CB');
+  console.log('%c     #######     ','color: #3CB7E4');
   console.log('')
-  console.log('###########    ');
-  console.log('############   ');
-  console.log('####     ####  ');
-  console.log('####      #### ');
-  console.log('####       ####');
-  console.log('####       ####');
-  console.log('####      #### ');
-  console.log('####     ####  ');
-  console.log('############   ');
-  console.log('###########    ');
+  console.log('%c###########    ','color: #004AE5');
+  console.log('%c############   ','color: #1956D2');
+  console.log('%c####     ####  ','color: #3262BF');
+  console.log('%c####      #### ','color: #4C6EAC');
+  console.log('%c####       ####','color: #657A9A');
+  console.log('%c####       ####','color: #7F8787');
+  console.log('%c####      #### ','color: #989374');
+  console.log('%c####     ####  ','color: #B19F62');
+  console.log('%c############   ','color: #CBAB4F');
+  console.log('%c###########    ','color: #FEC42A');
   }
   console.log('');
   console.log('Thanks for taking the time to check out my scripts.'); // add 'try calling funfunctio()....'
