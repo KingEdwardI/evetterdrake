7abe9c5f51ba65f1431f64a24dc54e2e97829b75
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2016-12-03 22:41:52 -0700

    colors

--- public/js/funstuff.js
+++ public/js/funstuff.js
@@ -1,16 +1,16 @@
 window.addEventListener('devtoolschange', function (e) {
 
   if(e.detail.orientation == "horizontal") {
-  console.log('%c##########    %c####         ####    %c###########', 'color: #0088E5','','');
-  console.log('%c##########    %c####         ####    %c############', 'color: #017DE5','','');
-  console.log('%c####          %c ####       ####     %c####     ####', 'color: #0273E5','','');
-  console.log('%c####          %c ####       ####     %c####      ####', 'color: #0369E6','','');
-  console.log('%c##########    %c  ####     ####      %c####       ####', 'color: #045EE6','','');
-  console.log('%c##########    %c  ####     ####      %c####       ####', 'color: #0654E7','','');
-  console.log('%c####          %c   ####   ####       %c####      ####', 'color: #074AE7','','');
-  console.log('%c####          %c   ####   ####       %c####     ####', 'color: #0830E7','','');
-  console.log('%c##########    %c    #########        %c############', 'color: #0A26E8','','');
-  console.log('%c##########    %c     #######         %c###########', 'color: #0C21E9','','');
+  console.log('%c##########    %c####         ####    %c###########',     'color: #8A00E5','color: #E50300','color: #004AE5');
+  console.log('%c##########    %c####         ####    %c############',    'color: #7F0CCE','color: #D21719','color: #1956D2');
+  console.log('%c####          %c ####       ####     %c####     ####',   'color: #7418B7','color: #BF2B32','color: #3262BF');
+  console.log('%c####          %c ####       ####     %c####      ####',  'color: #6924A0','color: #AC3F4C','color: #4C6EAC');
+  console.log('%c##########    %c  ####     ####      %c####       ####', 'color: #5E3089','color: #9A5365','color: #657A9A');
+  console.log('%c##########    %c  ####     ####      %c####       ####', 'color: #533D72','color: #87677F','color: #7F8787');
+  console.log('%c####          %c   ####   ####       %c####      ####',  'color: #48495B','color: #747B98','color: #989374');
+  console.log('%c####          %c   ####   ####       %c####     ####',   'color: #3D5544','color: #628FB1','color: #B19F62');
+  console.log('%c##########    %c    #########        %c############',    'color: #32612D','color: #4FA3CB','color: #CBAB4F');
+  console.log('%c##########    %c     #######         %c###########',     'color: #276D16','color: #3CB7E4','color: #FEC42A');
   console.log('');
   }
 
