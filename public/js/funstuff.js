1453d688c743383f0572eedbb55177b54d90bdaf
Edward Vetter-Drake <edward.vetterdrake@gmail.com>
2016-12-03 22:25:46 -0700

    added color

--- public/js/funstuff.js
+++ public/js/funstuff.js
@@ -1,16 +1,16 @@
 window.addEventListener('devtoolschange', function (e) {
 
   if(e.detail.orientation == "horizontal") {
-  console.log('##########    ####         ####    ###########');
-  console.log('##########    ####         ####    ############');
-  console.log('####           ####       ####     ####     ####');
-  console.log('####           ####       ####     ####      ####');
-  console.log('##########      ####     ####      ####       ####');
-  console.log('##########      ####     ####      ####       ####');
-  console.log('####             ####   ####       ####      ####');
-  console.log('####             ####   ####       ####     ####');
-  console.log('##########        #########        ############');
-  console.log('##########         #######         ###########');
+  console.log('%c##########    %c####         ####    %c###########', 'color: #0054E5','','');
+  console.log('%c##########    %c####         ####    %c############', 'color: #014EE5','','');
+  console.log('%c####          %c ####       ####     %c####     ####', 'color: #0249E5','','');
+  console.log('%c####          %c ####       ####     %c####      ####', 'color: #0344E6','','');
+  console.log('%c##########    %c  ####     ####      %c####       ####', 'color: #043FE6','','');
+  console.log('%c##########    %c  ####     ####      %c####       ####', 'color: #063AE7','','');
+  console.log('%c####          %c   ####   ####       %c####      ####', 'color: #0735E7','','');
+  console.log('%c####          %c   ####   ####       %c####     ####', 'color: #0830E7','','');
+  console.log('%c##########    %c    #########        %c############', 'color: #092BE8','','');
+  console.log('%c##########    %c     #######         %c###########', 'color: #0A26E8','','');
   console.log('');
   }
 
